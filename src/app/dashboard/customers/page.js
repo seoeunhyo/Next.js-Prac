7b05fbf9b02328c8aea0c32543b52f49f2643b88
seoeunhyo <seoeunhyo99@gmail.com>
2024-05-28 16:48:17 +0900

    [실습2] 디렉토리의 라우팅 확인해보기

---
+++ src/app/dashboard/customers/page.js
@@ -0,0 +1,10 @@
+export default function Customers(){
+    return (
+    <main className='flex min-h-screen flex-col items-center p-24'>
+      <div className='text-gray-500'>
+        이 페이지는 /dashboard 의 Layout을 계승받은 /dashboard/customers 페이지 입니다
+      </div>
+    </main>
+
+    )
+}
---
+++ src/app/dashboard/invoices/page.js
@@ -0,0 +1,9 @@
+export default function Invoices() {
+    return (
+      <main className='flex min-h-screen flex-col items-center p-24'>
+        <div className='text-gray-400'>
+          이 페이지는 /dashboard 의 Layout 을 계승받은 /dashboard/invoices 페이지 입니다
+        </div>
+      </main>
+    )
+  }
