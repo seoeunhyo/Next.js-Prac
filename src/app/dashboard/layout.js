0432551d577bcd69592db09cc75449c2d492819e
seoeunhyo <seoeunhyo99@gmail.com>
2024-05-28 16:44:31 +0900

    [실습1]

---
+++ src/app/dashboard/layout.js
@@ -0,0 +1,8 @@
+export default function DashboardLayout({children}){
+    return (
+        <>
+            <div className="w-full h-[100px] bg-red-500"></div>
+            {children}
+        </>
+    )
+}
---
+++ src/app/dashboard/page.js
@@ -0,0 +1,10 @@
+export default function Dashboard(){
+    return(
+        <main
+            className="flex min-h-screen flex-col items-center p-24"
+        >
+            <div className="text-red-500">Hello, world</div>
+            <div>확인하기 위한 Div 입니다.</div>
+        </main>
+    )
+}
