2803d178748731da4794a1aaafd05d13fbc5daed
seoeunhyo <seoeunhyo99@gmail.com>
2024-05-29 09:50:40 +0900

    [실습11] usePathname과 clsx 사용해서 현재 페이지 여부 확인하기

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -15,9 +15,9 @@ export default function DashboardLayout({children}){
             priority
           />
         </ClientMenuButton>
-        <ServerMenuButton href='/dashboard'>1. Dashboard</ServerMenuButton>
-        <ServerMenuButton href='/dashboard/invoices'>2. Invoices</ServerMenuButton>
-        <ServerMenuButton href='/dashboard/customers'>3. Customers</ServerMenuButton>
+        <ClientMenuButton href='/dashboard'>1. Dashboard</ClientMenuButton>
+        <ClientMenuButton href='/dashboard/invoices'>2. Invoices</ClientMenuButton>
+        <ClientMenuButton href='/dashboard/customers'>3. Customers</ClientMenuButton>
         </div>
         <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>
           <div className='w-full h-[100px] bg-red-500' />
--- src/app/dashboard/menu-client.js
+++ src/app/dashboard/menu-client.js
@@ -1,14 +1,20 @@
 'use client'
 
-import {useRouter} from 'next/navigation'
+import clsx from 'clsx'
+import {usePathname , useRouter} from 'next/navigation'
 
 export default function ClientMenuButton({href, children}){
+    const pathname = usePathname()
     const router = useRouter()
+    const isCurrentPage = pathname === href
 
     return (
         <div
             onClick={()=> router.push(href)}
-            className='border-2 border-solid p-[10px] border-white block'
+            className={clsx(
+                'border-2 border-solid p-[10px] block',
+                isCurrentPage ? 'border-red-500': 'border-white'
+            )}
         >
             {children}
         </div>
